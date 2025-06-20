7b680575ebbed110ba0ab936f8aafd427b3738ee
kevinchou7 <kevinchou1207@outlook.com>
2025-06-20 21:12:17 +0800

    Update gradebook.c
    Answer

--- homework/hw2/gradebook.c
+++ homework/hw2/gradebook.c
@@ -15,35 +15,85 @@ unsigned hash(const char *str) {
     return hash_val % NUM_BUCKETS;
 }
 
-gradebook_t *create_gradebook(const char *class_name) {
+gradebook_t* create_gradebook(const char* class_name) {
     // TODO: craete a new book
-
+    gradebook_t* book = malloc(sizeof(gradebook_t));
+    if (book == NULL) {
+        return NULL;
+    }
     // initialize the gradebook
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        book->buckets[i] = NULL;
+    }
     // strcpy book->class_name
+    strcpy(book->class_name, class_name);
+    return book;
+}
+
+const char* get_gradebook_name(const gradebook_t* book) {
+    return book->class_name;
+}
+
+int add_score(gradebook_t* book, const char* name, int score) {
+    //if book or name doesn't exist, return false
+    if (book == NULL || name == NULL) {
+        return -1;
+    }
+    //if the name exists, renew the score
+    unsigned idx = hash(name);
+    node_t* curr = book->buckets[idx];
+    //refer to the score, change the score
+    while (name != NULL) {
+        if (strcmp(curr->name, name) == 0) {
+            curr->score = score;
+            return 0;
+        }
+        curr = curr->next; //traverse the list to find the corresponding name
+    }
+    return 0;
+}
+
+int find_score(const gradebook_t* book, const char* name) {
+    if (book == NULL || name == NULL) {
+        return -1;
+    }
     
-}
+    unsigned idx = hash(name);
+    node_t* curr = book->buckets[idx];
 
-const char *get_gradebook_name(const gradebook_t *book) {
-    // TODO Not yet implemented
-    return NULL;
-}
-
-int add_score(gradebook_t *book, const char *name, int score) {
-    // TODO Not yet implemented
-    return 0;
-}
-
-int find_score(const gradebook_t *book, const char *name) {
-    // TODO Not yet implemented
-    return 0;
+    while (curr != NULL) {
+        if (strcmp(curr->name, name) == 0) {
+            return curr->score;
+        }
+        curr = curr->next;
+    }
+    return -1;
 }
 
 void print_gradebook(const gradebook_t *book) {
-    // TODO Not yet implemented
+    printf("%s\n", book->class_name);
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        node_t* curr = book->buckets[i];
+        while (curr != NULL) {
+            printf("Student: %s, Score: %d\n", curr->name, curr->score);
+            curr = curr->next;
+        }
+    }
 }
 
 void free_gradebook(gradebook_t *book) {
-    // TODO Not yet implemented
+    if (book == NULL) {
+        return;
+    }
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        node_t* curr = book->buckets[i];
+        while (curr != NULL) {
+            node_t* temp = curr;
+            curr = curr->next;
+            free(temp);
+        }
+    }
+    free(book);
 }
 
 int write_gradebook_to_text(const gradebook_t *book) {
@@ -96,5 +146,3 @@ gradebook_t *read_gradebook_from_text(const char *file_name) {
     pf = NULL;
     return new_book;
 }
-
-
