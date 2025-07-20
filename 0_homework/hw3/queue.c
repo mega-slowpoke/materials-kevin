1eebf0606de1a900f0da8952c7de64da00233b26
chaoxu <wuchaoxu1006@gmail.com>
2025-07-20 13:21:05 -0700

    update hw3

--- 0_homework/hw3/queue.c
+++ 0_homework/hw3/queue.c
@@ -3,8 +3,9 @@
 
 const int INITIAL_SIZE = 6;
 typedef struct ArrayQueue {
-    int arr[INITIAL_SIZE];
-    int size;
+    int* arr;
+    int size;     // 当前元素个数
+    int capacity; // 当前arr最大的容量
     int nextInsertIdx;
     int frontIdx;
 } ArrayQueue;
@@ -12,6 +13,9 @@ typedef struct ArrayQueue {
 
 // 初始化ArrayQueue, 上面的这些字段应该怎么赋值?
 ArrayQueue* createArrayQueue() { 
+    ArrayQueue* queue = (ArrayQueue*)malloc(sizeof(ArrayQueue));
+    queue->arr = (int*)malloc(sizeof(int) * INITIAL_SIZE);
+    // TODO: 初始化剩下的字段
 }
 
 void enqueue(ArrayQueue* queue, int val) { 
@@ -21,7 +25,7 @@ void enqueue(ArrayQueue* queue, int val) {
 
     // 2.有足够大小
     queue->arr[queue->nextInsertIdx] = val;
-    queue->nextInsertIdx++;
+    queue->nextInsertIdx = (queue->nextInsertIdx + 1) % (queue->capacity);
     queue->size++;
 }
 
